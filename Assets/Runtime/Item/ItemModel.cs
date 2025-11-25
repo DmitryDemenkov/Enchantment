d3fab4d9ee858a4aea8301281441c6f993a6a8b6
DmitryDemenkov <demon.krut2002@mail.ru>
2025-11-25 14:29:37 +0300

    [Rename] Item states ro stats

--- Assets/Runtime/Item/ItemModel.cs
+++ Assets/Runtime/Item/ItemModel.cs
@@ -11,25 +11,25 @@ public class ItemModel
     private int _level;
     public int Level { get { return _level; } }
 
-    private IReadOnlyDictionary<string, int> _states;
-    public IReadOnlyDictionary<string, int> States { get { return _states; } }
+    private IReadOnlyDictionary<string, int> _stats;
+    public IReadOnlyDictionary<string, int> Stats { get { return _stats; } }
 
 
     public ItemModel(string itemType, int itemLevel, IReadOnlyDictionary<string, int> itemProperties)
     {
         _item = itemType;
         _level = itemLevel;
-        _states = itemProperties;
+        _stats = itemProperties;
     }
 
     public void Modify(IReadOnlyDictionary<string, int> itemProperties)
     {
         var modifyItemProperties = new Dictionary<string, int>(itemProperties);
 
-        foreach (var pair in _states)
+        foreach (var pair in _stats)
             modifyItemProperties[pair.Key] = modifyItemProperties.GetValueOrDefault(pair.Key) + pair.Value;
 
-        _states = modifyItemProperties;
+        _stats = modifyItemProperties;
         _level++;
 
         StatsChanged?.Invoke();
