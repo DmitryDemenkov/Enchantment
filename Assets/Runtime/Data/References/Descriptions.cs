1384fd3a0f59292c433a147d93856d57b07f128f
DmitryDemenkov <demon.krut2002@mail.ru>
2025-11-26 14:09:52 +0300

    [Add] Enchantment locig in new model

--- Assets/Runtime/Data/References/Descriptions.cs
+++ Assets/Runtime/Data/References/Descriptions.cs
@@ -5,18 +5,18 @@ public class Descriptions
 {
     public ItemDescriptionCollection Items { get; }
 
-    [Include]
-    [DecodeAlias("chances")]
-    private List<float> _chances;
+    public List<float> Chances { get; }
 
     public Descriptions(Variant variant)
     {
         var v_items = (ProxyObject)variant["items"];
         Items = new ItemDescriptionCollection("items", v_items);
+
+        Chances = variant["chances"].Make<List<float>>();
     }
 
     public IReadOnlyList<float> GetChances()
     {
-        return _chances;
+        return Chances;
     }
 }
